4271ed8ba14b7e6be02cd1d4a387a1764f135a6f
Abhishek777777777 <abhishekmishra992016@gmail.com>
2024-02-11 10:40:22 +0530

    Build

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import { Avatar, Typography } from '@mui/material'
+import { Avatar } from '@mui/material'
 import React from 'react'
 
 const Profile = () => {
@@ -7,7 +7,7 @@ const Profile = () => {
             <h3>Account Settings</h3>
             <div className='profile__page'>
                 <Avatar
-                    alt="Remy Sharp"
+                    alt="Your image"
                     src="https://tse1.mm.bing.net/th?id=OIP.0Eq0sjsuS1d_KLs4pc2vTgHaLH&pid=Api&rs=1&c=1&qlt=95&w=73&h=110"
                     sx={{ width: 56, height: 56 }}
                 />
